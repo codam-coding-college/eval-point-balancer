10066745b89e6dd01f9d9e1ca310ba8ac6b77426
Freek Bes <freek.bes@codam.nl>
2026-08-05 15:58:21 +0200

    fix: update dependencies

--- release.config.js
+++ release.config.js
@@ -4,6 +4,25 @@ module.exports = {
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
+		[
+			"@semantic-release/exec",
+			{
+				publishCmd: 'docker tag ${REGISTRY}/${IMAGE_NAME} ${REGISTRY}/${IMAGE_NAME}:${nextRelease.version} && docker push ${REGISTRY}/${IMAGE_NAME}:${nextRelease.version}'
+			}
+		],
+		[
+			"@semantic-release/npm",
+			{
+				npmPublish: false
+			}
+		],
 		"@semantic-release/github",
+		[
+			"@semantic-release/git",
+			{
+				assets: ["package.json"],
+				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+			}
+		]
 	]
 };
